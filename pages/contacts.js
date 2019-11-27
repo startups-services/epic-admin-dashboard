d9c555e32b403ca0928494c184890354e5357a73
pks <pks1989@ya.ru>
2019-11-27 15:14:41 +0700

    fix error with sign up

--- pages/contacts.js
+++ pages/contacts.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import App from '../components/App';
+import pageInitialData from '../components/_Utility/pageInitialData';
 
 const Contacts = () => (
   <App>
     <h1>Contacts</h1>
   </App>
 );
+Index
+  .getInitialProps = async ({ reduxStore, res, req }) => pageInitialData({ reduxStore, res, req });
 
 export default Contacts;

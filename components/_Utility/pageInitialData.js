5163e1f5e3c663fcdffe2d7e17c9a489d23aafb5
pks1989 <codeforlemontea@gmail.com>
2019-12-08 20:28:32 +0700

    little fix

--- components/_Utility/pageInitialData.js
+++ components/_Utility/pageInitialData.js
@@ -1,10 +1,12 @@
 import { getInitialProjects } from '../../redux/projects/actions';
 import { getAllUsers } from '../../redux/users/actions';
 import auth0 from '../../utils/auth0';
+import { getInitialUserData } from '../../redux/activeUser/actions';
 
 const pageInitialData = async ({ req, res, reduxStore }) => {
   if (typeof window === 'undefined') {
     const result = await auth0.getSession(req);
+    debugger;
     if (!result) {
       res.writeHead(302, {
         Location: '/api/login',
@@ -15,7 +17,11 @@ const pageInitialData = async ({ req, res, reduxStore }) => {
     const { dispatch } = reduxStore;
     await dispatch(getInitialProjects(res));
     await dispatch(getAllUsers());
+    await dispatch(getInitialUserData(result.user));
+
+    return result.user;
   }
+  return {};
 };
 
 export default pageInitialData;
